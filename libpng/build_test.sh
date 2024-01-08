7f122adfb25bcbc620a4b52ada46b0b586ef7b7b
winechan <chen2002aa@163.com>
2024-01-08 15:11:46 +0800

    libpng

---
+++ libpng/build_test.sh
@@ -0,0 +1,15 @@
+if [ -n "$ANDROID_NDK" ]; then
+    export NDK=${ANDROID_NDK}
+elif [ -n "$ANDROID_NDK_HOME" ]; then
+    export NDK=${ANDROID_NDK_HOME}
+elif [ -n "$ANDROID_NDK_HOME" ]; then
+    export NDK=${ANDROID_NDK_HOME}
+else
+    export NDK=~/android-ndk-r15c
+fi
+
+if [ ! -d "$NDK" ]; then
+    echo "Please set ANDROID_NDK environment to the root of NDK."
+    exit 1
+fi
+"$NDK"/ndk-build NDK_PROJECT_PATH=./ $@
